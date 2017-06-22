0e7cae5244e9681189df093b8bd2f081449d3e54
pablofullana <pablofullana@gmail.com>
2017-06-22 18:36:31 -0300

    Add server-setup endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       get :info, to: 'static#info'
+      get :server_setup, to: 'static#server_setup'
     end
   end
 end
