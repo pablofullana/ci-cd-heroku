a664845c208a53b853e701c4a797b39676f62b96
Pablo Fullana <pablofullana@gmail.com>
2017-06-22 18:40:24 -0300

    Add server-setup endpoint (#2)

--- app/controllers/api/v1/static_controller.rb
+++ app/controllers/api/v1/static_controller.rb
@@ -4,6 +4,10 @@ module Api
       def info
         render json: { version: '0.1' }, status: :ok
       end
+
+      def server_setup
+        render json: { rails: '5.1.1' }, status: :ok
+      end
     end
   end
 end
