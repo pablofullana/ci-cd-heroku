c93296fcbb2c4ecdcddca36d1673db57803a5091
pablofullana <pablofullana@gmail.com>
2017-06-23 15:14:16 -0300

    Add description to settings

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170623172238) do
+ActiveRecord::Schema.define(version: 20170623181330) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20170623172238) do
     t.string "value"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "description"
   end
 
 end
