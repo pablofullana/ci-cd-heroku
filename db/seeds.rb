55e31136620ce11c1ff4d068d0a8e2454ee76c86
pablofullana <pablofullana@gmail.com>
2017-06-23 15:39:30 -0300

    Fix typo in seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Setting.crate(
+Setting.create(
   name: 'version',
   value: '0.0.1',
   description: 'Seeded setting'
