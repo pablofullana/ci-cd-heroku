37bc1228f8d5d68359617fd008626e79c450dd5e
Pablo Fullana <pablofullana@gmail.com>
2017-06-23 14:51:48 -0300

    Create Settings (#3)
    * Create Settings
    
    * Add Settings index endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       get :info, to: 'static#info'
       get :server_setup, to: 'static#server_setup'
+      resources :settings, only: [:index]
     end
   end
 end
